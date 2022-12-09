d9288ec37185e06e4d3f610991616f917fae0682
CrismarCR11 <crismar_tk_@hotmail.com>
2022-12-09 16:31:07 -0400

    segunda subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,30 @@ const routes: Routes = [
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
+  /*{
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'customers',
+    loadChildren: () => import('./customers/customers.module').then( m => m.CustomersPageModule)
+  },*/
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'categoria',
+    loadChildren: () => import('./categoria/categoria.module').then( m => m.CategoriaPageModule)
+  },
+  /*{
+    path: 'producto',
+    loadChildren: () => import('./producto/producto.module').then( m => m.ProductoPageModule)
+  }*/
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'login', url: 'login', icon: 'people' },
-   
+    { title: 'home', url: '/home', icon: 'home' },
+    { title: 'customers', url: '/customers', icon: 'people' },
+    { title: 'login', url: '/login', icon: 'people' },
+    { title: 'registro', url: '/registro', icon: 'people' },
+    { title: 'categoria', url: '/categoria', icon: 'category' },
+    { title: 'producto', url: '/producto', icon: 'people' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
