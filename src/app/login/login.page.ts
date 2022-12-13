5e7c273fe6ca5b3df54ad01432a79ea91f382b72
CrismarCR11 <crismar_tk_@hotmail.com>
2022-12-13 18:55:45 -0400

    agregados nuevos items

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import {
   Validators,
   FormBuilder
 } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -14,7 +15,9 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) { 
 
     this.formularioLogin = this.fb.group({
       'nombre': new FormControl("",Validators.required),
@@ -24,6 +27,33 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = localStorage.getItem('usuario');
+
+    //const dato = localStorage.getItem('usuario');
+    //Solo entra si no es nulo. 
+    if(usuario) 
+    {var dato=JSON.parse(usuario);}
+    //ejemplo de resolución.
+    
+    console.log(dato);
+    if(dato.nombre == f.nombre && dato.password == f.password){
+      console.log('Ingresado');
+      localStorage.setItem('ingresado','true');
+      this.navCtrl.navigateRoot('home');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos incorrectos',
+        message: 'Los datos que ingresaste son incorrectos.',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+    }
   }
 
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -6,9 +6,25 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   items: any[] = [
+    { id: 3, name: 'Netflix', price: 700, category: 'USA', img: 'assets/imgs/netflix.png' },
+    { id: 3, name: 'Disney Plus', price: 700, category: 'USA', img: 'assets/imgs/disney-plus.jpg' },
+    { id: 3, name: 'Star Plus', price: 700, category: 'USA', img: 'assets/imgs/star+.png' },
+    { id: 3, name: 'Iphone 11', price: 700, category: 'USA', img: 'assets/imgs/iphone11.jpg' },
     { id: 1, name: 'Aloe Plant', price: 700, category: 'INDIA', img: 'assets/imgs/aloe.png' },
     { id: 2, name: 'Echeveria', price: 500, category: 'INDIA', img: 'assets/imgs/2.png' },
     { id: 3, name: 'Snake Plant', price: 700, category: 'USA', img: 'assets/imgs/3.png' },
+  ];
+
+  algo: any[] = [
+    { id: 1, name: 'nose', price: 700, category: 'INDIA', img: 'assets/imgs/aloe.png' },
+    { id: 2, name: 'vamos', price: 500, category: 'INDIA', img: 'assets/imgs/2.png' },
+    { id: 3, name: 'xd', price: 700, category: 'USA', img: 'assets/imgs/3.png' },
+  ];
+
+  usuarios: any[] = [
+    { id: 1, name: 'Rodrigo', telefono: 78945678, correo: 'rod@gmail.com' },
+    { id: 2, name: 'Carlos', telefono: 78945678, correo: 'carlos@gmail.com' },
+    { id: 3, name: 'Carolina', telefono: 78945678, correo: 'Carolina@gmail.com' },
   ];
 
   constructor() { }
